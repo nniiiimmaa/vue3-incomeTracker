9e9ec1a6f486c1ab4dca3aeea8c54b5a4271bb94
nniimma <nimakhazforoosh@yahoo.com>
2026-04-28 20:47:12 -0300

    adding yml to the project

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: '/vue3-incomeTracker/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
